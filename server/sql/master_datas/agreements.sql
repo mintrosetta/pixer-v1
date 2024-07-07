136ae4a390d0dd27b66c97057736f4a685090959
Mint Rosetta <mint.rosetta2001@gmail.com>
2024-07-07 15:27:42 +0700

    add master data

---
+++ server/sql/master_datas/agreements.sql
@@ -0,0 +1,2 @@
+INSERT INTO agrements (name) VALUES ('เชิงพาณิชย์');
+INSERT INTO agrements (name) VALUES ('ขายต่อ');
---
+++ server/sql/master_datas/user_roles.sql
@@ -0,0 +1,2 @@
+INSERT INTO user_roles (name) VALUES ('admin');
+INSERT INTO user_roles (name) VALUES ('member');
